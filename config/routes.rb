729807abbabe927b012e9e44a90a7c04e56db471
Johnny Shields <johnny.shields@gmail.com>
2014-04-05 03:41:18 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HealthCheck::Engine.routes.draw do
 
   root :to => 'health_check#index'
 
-  get 'health_check(/:checks)(.:format)',
+  get '(/:checks)(.:format)',
       :to => 'health_check/health_check#index',
       :as => 'health_check'
 
